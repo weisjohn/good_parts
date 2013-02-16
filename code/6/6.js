9f4da26a6929f9b1e42808cbf0e4b24e15333238
John Weis <weis.john@gmail.com>
2013-02-15 19:40:39 -0500

    fixing a bug with the leftReduce functiong

--- code/6/6.js
+++ code/6/6.js
@@ -4,12 +4,20 @@
 Array.method('leftReduce', function (f, value) {
     // if (typeof value === "undefined") { value = "" }
     var i;
-    for (i = this.length; i >= 0; i -= 1) {
+    for (i = this.length-1; i >= 0; i--) {
         value = f(this[i], value);
         console.log(value);
     }
     return value;
 });
+
+
+var add = function(a, b) {
+    return a + b;
+}
+
+var scores = [1,2,3,4,5];
+scores.leftReduce(add, 0);
 
 
 var diffs = function(a, b) {
--- code/6/7.js
+++ code/6/7.js
@@ -9,13 +9,14 @@ String.prototype.repeat = function(length) {
 }
 
 Array.method('dimensional', function (m, n, initial) {
+    console.log('\t'.repeat(m), 'step ', m);
     if (typeof m === "number" && typeof n === "number") {} else { return; }
     
-    var arr = []
+    var arr = [];
     for (var i = 0; i < n; i++) {
         // on the last dimension, just expand the array
         if (m > 2) {
-            arr[i] = Array.prototype.dimensional(m - 1, n, initial);
+            arr[i] = [].dimensional(m - 1, n, initial);
         } else {
             arr[i] = eval( "[" + ",".repeat(n) + "]" );
             if (initial) {
@@ -29,6 +30,6 @@ Array.method('dimensional', function (m, n, initial) {
 });
 
 
-var rubiks = [].dimensional(3, 3, 'red');
+var rubiks = [].dimensional(3, 3, '0');
 
 console.log(rubiks);
