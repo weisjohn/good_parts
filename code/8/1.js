c2ce0845e1f048f51a677d6135022daa0cafab00
John Weis <weis.john@gmail.com>
2013-02-15 23:14:36 -0500

    finished chapter 8

--- code/8/1.js
+++ code/8/1.js
@@ -1,2 +1,14 @@
-// methods
+// numbers
+var rand = Math.random();
 
+// toExponential 
+console.log('toExponential', (rand).toExponential(7) );
+
+// toFixed 
+console.log('toFixed', (rand).toFixed(7) );
+
+// toPrecision
+console.log('toPrecision', (rand).toPrecision() );
+
+// toString
+console.log('toString', (rand).toString('1') );
---
+++ code/8/2.js
@@ -0,0 +1,12 @@
+// strings
+
+console.log('the code for "¥" is', "¥".charCodeAt(0));
+
+console.log('conc'.concat('a', 't'));
+var a=2,b=3;
+console.log(''.concat(a,b));
+
+console.log('1509e0dc-23ff-897c-0c32-7f97b53df350'.split('-', 3));
+
+var a = String.fromCharCode(67, 97, 116);
+console.log(a);
